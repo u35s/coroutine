5d242f6cdbeb6b837cc9841727fc95695e31dace
u35s <jintao7080@gmail.com>
2018-12-19 12:09:59 +0800

    添加注释

--- coroutine.go
+++ coroutine.go
@@ -33,11 +33,9 @@ func (co *Coroutine) Run(exe func() error) error {
 	go func() {
 		err := exe()
 		if co.mRunDone != nil {
-			ticker := time.NewTicker(co.mWaitDuration)
-			defer ticker.Stop()
 			select {
 			case co.mRunDone <- err:
-			case <-ticker.C:
+			default: // Yield 之后mRunDone <- err将会阻塞
 			}
 		}
 	}()
@@ -65,7 +63,8 @@ func (co *Coroutine) Resume() {
 	defer ticker.Stop()
 	select {
 	case <-co.mDone:
-	case <-ticker.C:
+	case <-ticker.C: // 当coroutine长时间不调用Done,tick.C返回导致程序并发,把tick设置的大一点
+		// 当Resume后并发不会有问题可以去掉此处的select阻塞
 	}
 }
 
